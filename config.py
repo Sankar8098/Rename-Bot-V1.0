9331f72f388ab3f7a0747ea8c310545ef83a6aba
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-02-05 18:23:29 +0530

    Update config.py
    Signed-off-by: Sankar8098 <113809269+Sankar8098@users.noreply.github.com>

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ API_ID = os.environ.get("API_ID", "23990433")
 
 API_HASH = os.environ.get("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5964494060:AAFfOEerT8lR8YqyEHJQtbFQidqkqkYUaO8") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "SK_MoviesOffl") 
 
-DB_NAME = os.environ.get("DB_NAME","5964494060:AAFfOEerT8lR8YqyEHJQtbFQidqkqkYUaO8")     
+DB_NAME = os.environ.get("DB_NAME","sankar")     
 
 DB_URL = os.environ.get("DB_URL","mongodb+srv://Test:1234@cluster0.2bzsp0q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
  
